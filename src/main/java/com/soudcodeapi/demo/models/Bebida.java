66f95bfb2aec95c38a1f1b140a3ba448d46e6c71
DoriansFer <doohfernandes@yahoo.com>
2024-05-02 19:42:29 -0300

    getters e setters

--- src/main/java/com/soudcodeapi/demo/models/Bebida.java
+++ src/main/java/com/soudcodeapi/demo/models/Bebida.java
@@ -20,4 +20,36 @@ public class Bebida {
 
     @ManyToMany(mappedBy = "bebidas")
     private Set<Pedido> pedidos = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Set<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(Set<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
 }
